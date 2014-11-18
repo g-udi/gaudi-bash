ba940ebffdbce45bd3eeee4a4ac7ee906dfff1db
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2014-11-18 17:41:29 +0100

    changing pyenv and node plugins and install new themes

--- plugins/available/pyenv.plugin.bash
+++ plugins/available/pyenv.plugin.bash
@@ -10,5 +10,54 @@ if pyenv virtualenv-init - &> /dev/null; then
   eval "$(pyenv virtualenv-init -)"
 fi
 
+function mkpvenv {
+  about 'create a new virtualenv for this directory'
+  group 'pyenv-virtualenv'
+
+  cwd=`basename \`pwd\``
+  eval "touch .env"
+  eval "echo \"#!/bin/bash\" >> .env"
+  eval "echo \"if [ \\\`basename \\\$(pwd)\\\` == \\\"$cwd\\\" ]; then \"eval \"wopvenv\"\"; fi\" >> .env"
+  mkvirtualenv --distribute $cwd
+}
+
+function mkpvbranch {
+  about 'create a new virtualenv for the current branch'
+  group 'pyenv-virtualenv'
+
+  mkvirtualenv --distribute "$(basename `pwd`)@$SCM_BRANCH"
+}
+
+function wopvbranch {
+  about 'sets workon branch'
+  group 'pyenv-virtualenv'
+
+  workon "$(basename `pwd`)@$SCM_BRANCH"
+}
+
+function wopvenv {
+  about 'works on the virtualenv for this directory'
+  group 'virtualenv'
+
+  workon "$(basename `pwd`)"
+}
+
+function rmpvenv {
+  about 'removes virtualenv for this directory'
+  group 'virtualenv'
+
+  eval "deactivate"
+  rmvirtualenv "$(basename `pwd`)"
+  eval "rm .env"
+}
+
+function rmpvenvbranch {
+  about 'removes virtualenv for this directory'
+  group 'virtualenv'
+
+  eval "deactivate"
+  rmvirtualenv "$(basename `pwd`)@$SCM_BRANCH"
+}
+
 # Load the auto-completion script if pyenv was loaded.
 [[ -e $PYENV_ROOT/completions/pyenv.bash ]] && source $PYENV_ROOT/completions/pyenv.bash
