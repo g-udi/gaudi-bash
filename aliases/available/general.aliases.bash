560fb4058c5da43c2af4079a3444b27f3c7c2107
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2014-11-19 03:38:02 +0100

    finising workflow and customization

--- aliases/available/general.aliases.bash
+++ aliases/available/general.aliases.bash
@@ -14,30 +14,11 @@ alias ls="command ls ${colorflag}"
 alias l='ls -a'
 alias l1='ls -1'
 
-export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-
-# Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-	colorflag="--color"
-else # OS X `ls`
-	colorflag="-G"
-fi
-
 alias _="sudo"
 # Enable aliases to be sudo’ed
 alias sudo='sudo '
 # Get week number
 alias week='date +%V'
-
-if [ $(uname) = "Linux" ]
-then
-  alias ls="ls --color=auto"
-fi
-which gshuf &> /dev/null
-if [ $? -eq 0 ]
-then
-  alias shuf=gshuf
-fi
 
 alias c='clear'
 alias k='clear'
@@ -45,15 +26,11 @@ alias cls='clear'
 
 alias edit="$EDITOR"
 alias pager="$PAGER"
-
 alias q='exit'
-
 alias irc="$IRC_CLIENT"
-
 alias rb='ruby'
 
 # Pianobar can be found here: http://github.com/PromyLOPh/pianobar/
-
 alias piano='pianobar'
 
 # Easier navigation: .., ..., ...., ....., ~ and -
@@ -76,3 +53,9 @@ fi
 # Directory
 alias	md='mkdir -p'
 alias	rd='rmdir'
+
+which gshuf &> /dev/null
+if [ $? -eq 0 ]
+then
+  alias shuf=gshuf
+fi
--- themes/sexy/sexy.theme.bash
+++ themes/sexy/sexy.theme.bash
@@ -33,7 +33,7 @@ else
 fi
 
 parse_git_dirty () {
-  [[ $(git status 2> /dev/null | tail -n1 | cut -c 1-17) != "nothing to commit" ]] && echo "*"
+  [[ $(git status 2> /dev/null | tail -n1 | cut -c 1-17) != "nothing to commit" ]] && echo " +"
 }
 parse_git_branch () {
   git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/\1$(parse_git_dirty)/"
