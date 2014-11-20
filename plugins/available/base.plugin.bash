722c32f1d9d47aac732974c3dbcb1d08bc930539
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2014-11-20 15:36:37 +0100

    updating repo from update script

--- plugins/available/base.plugin.bash
+++ plugins/available/base.plugin.bash
@@ -244,3 +244,12 @@ function o() {
 function tre() {
     tree -aC -I '.git|node_modules|bower_components' --dirsfirst "$@" | less -FRNX;
 }
+
+# print the available colors palette
+function colors() {
+    color=16;
+    while [ $color -lt 245 ]; do
+        echo -e "$color: \\033[38;5;${color}mhello\\033[48;5;${color}mworld\\033[0m"
+        ((color++));
+    done
+}
