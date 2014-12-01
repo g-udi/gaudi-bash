579397c23b709d9da3399b485a0d54bc44fcabdf
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2014-12-01 17:00:24 +0100

    adding documentation to encode plugin

--- plugins/available/encode.plugin.bash
+++ plugins/available/encode.plugin.bash
@@ -1,5 +1,10 @@
-# UTF-8-encode a string of Unicode symbols
+cite about-plugin
+about-plugin 'Encoding and decoding functions'
+
 function escape() {
+    about 'UTF-8-encode a string of Unicode symbols'
+    group 'encode'
+
     printf "\\\x%s" $(printf "$@" | xxd -p -c1 -u);
     # print a newline unless we’re piping the output to another program
     if [ -t 1 ]; then
@@ -7,8 +12,10 @@ function escape() {
     fi;
 }
 
-# Decode \x{ABCD}-style Unicode escape sequences
 function unidecode() {
+    about 'Decode \x{ABCD}-style Unicode escape sequences'
+    group 'encode'
+
     perl -e "binmode(STDOUT, ':utf8'); print \"$@\"";
     # print a newline unless we’re piping the output to another program
     if [ -t 1 ]; then
