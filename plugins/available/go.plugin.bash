255e5306052ff174a81aa20ede7fbd52b5f0f2dc
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2018-10-09 18:42:04 +0100

    Adding setting up GOPATH from the go plugin

--- plugins/available/go.plugin.bash
+++ plugins/available/go.plugin.bash
@@ -5,6 +5,7 @@ about-plugin 'go environment variables & path configuration'
 
 [ ! command -v go &>/dev/null ] && return
 
+[ -d "$HOME/Applications/Go" ] && mkdir -p "$HOME/Applications/Go"
 export GOPATH="$HOME/Applications/Go"
 
 export GOROOT=${GOROOT:-$(go env | grep GOROOT | cut -d'"' -f2)}
