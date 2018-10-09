6ae9b33be066fa112c438d0a5f4e6125c067cb79
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2018-10-09 18:07:31 +0100

    Adding man coloring and config params

---
+++ aliases/available/applications.aliases.bash
@@ -0,0 +1,7 @@
+# Assign the default editor and Git editor
+export EDITOR="vi";
+export GIT_EDITOR="vi";
+# Change this to your console based IRC client of choice
+export IRC_CLIENT='irssi'
+# Set this to the command you use for todo program CLI
+export TODO="todoist"
