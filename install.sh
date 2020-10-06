dec9dd260092f2788cec2163dfccd25c312ad977
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2020-10-06 03:54:59 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,22 +15,22 @@ if [[ -d "$BASH_IT" ]]; then
     done
     if [[ $REPLY =~ ^[yY]$ ]]; then
         rm -rf "$BASH_IT"
-
-        # Prevent the cloned repository from having insecure permissions. Failing to do
-        # so causes compinit() calls to fail with "command not found: compdef" errors
-        # for users with insecure umasks (e.g., "002", allowing group writability). Note
-        # that this will be ignored under Cygwin by default, as Windows ACLs take
-        # precedence over umasks except for filesystems mounted with option "noacl".
-        umask g-w,o-w
-
-        env git clone --depth=1 --recurse-submodules https://github.com/ahmadassaf/bash-it.git "$BASH_IT" || {
-            printf "Error: Cloning of gaudi into this machine failed :(\\n"
-            exit 1
-        }
-
-        bash "$BASH_IT/setup.sh"
     else
         echo "Running a bash-it update to pull latest changes ..."
         git -C "$BASH_IT" pull
     fi
+else
+    # Prevent the cloned repository from having insecure permissions. Failing to do
+    # so causes compinit() calls to fail with "command not found: compdef" errors
+    # for users with insecure umasks (e.g., "002", allowing group writability). Note
+    # that this will be ignored under Cygwin by default, as Windows ACLs take
+    # precedence over umasks except for filesystems mounted with option "noacl".
+    umask g-w,o-w
+
+    env git clone --depth=1 --recurse-submodules https://github.com/ahmadassaf/bash-it.git "$BASH_IT" || {
+        printf "Error: Cloning of gaudi into this machine failed :(\\n"
+        exit 1
+    }
+
+    bash "$BASH_IT/setup.sh"
 fi
