a4926615d0c9f33331db17c98f9a2cd20e7766a9
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2020-10-12 12:21:55 -0700

    Removed submodule commit message

--- bin/bats-assert/test/test_helper.bash
+++
@@ -1,7 +0,0 @@
-setup () {
-  export TEST_MAIN_DIR="${BATS_TEST_DIRNAME}/.."
-  export TEST_DEPS_DIR="${TEST_DEPS_DIR-${TEST_MAIN_DIR}/..}"
-
-  load "${TEST_DEPS_DIR}/bats-support/load.bash"
-  load '../load'
-}
