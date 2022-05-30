640ad1cdb0cf9ff27ab2b17e287cf3d99f624ccc
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2022-05-30 02:09:59 +0100

    Carry install parameters to the setup

--- setup.sh
+++ setup.sh
@@ -124,7 +124,7 @@ cite about param example group priority
 source "$GAUDI_BASH/lib/gaudi-bash.bash"
 
 # Check if the folder is a valid git and pull all submodules
-[[ -d "$GAUDI_BASH/.git" ]] && git submodule update --init --recursive
+# [[ -d "$GAUDI_BASH/.git" ]] && git submodule update --init --recursive
 
 if [[ "$no_default_components" != "true" ]]; then
 
