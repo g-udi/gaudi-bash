21a7a8af94e8b56cdeb078a26587b9c201ce9ca1
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2024-02-22 18:27:37 +0000

    Fix references to gaudi-bash

--- template/bash_profile.template.bash
+++ template/bash_profile.template.bash
@@ -13,12 +13,9 @@ unset MAILCHECK
 # Set this to false to turn off version control status checking within the prompt for all themes
 export SCM_CHECK=true
 
-# (Advanced): Uncomment this to make Bash-it reload itself automatically
+# (Advanced): Uncomment this to make gaudi-bash reload itself automatically
 # After enabling or disabling aliases, plugins, and completions.
 export GAUDI_BASH_AUTOMATIC_RELOAD_AFTER_CONFIG_CHANGE=1
-
-# Uncomment this to make Bash-it create alias reload [ref: https://github.com/Bash-it/gaudi-bash/issues/1120]
-export GAUDI_BASH_RELOAD_LEGACY=0
 
 # Uncomment this to show gaudi-bash debug logs
 # export GAUDI_BASH_LOG_LEVEL=3
