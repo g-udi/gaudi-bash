0255c62f21b5b19f20d10700bc06b4e804d163f3
Ahmad Assaf <ahmad.a.assaf@gmail.com>
2024-02-22 18:30:11 +0000

    Update the history function

--- lib/history.bash
+++ lib/history.bash
@@ -1,10 +1,28 @@
 #!/usr/bin/env bash
 # shellcheck shell=bash
 
-shopt -s histappend                                      # append to bash_history if Terminal.app quits
-export HISTCONTROL=${HISTCONTROL:-ignorespace:erasedups} # erase duplicates; alternative option: export HISTCONTROL=ignoredups
-export HISTSIZE=${HISTSIZE:-5000}                        # resize history size
-export AUTOFEATURE=${AUTOFEATURE:-true autotest}         # cucumber / Autotest integration
+shopt -s histappend
+
+if [[ ${BASH_VERSINFO[0]} -lt 4 ]] || [[ ${BASH_VERSINFO[0]} -eq 4 && ${BASH_VERSINFO[1]} -lt 3 ]]; then
+	_log_warning "Bash version 4.3 introduced the 'unlimited' history size capability."
+	return 1
+fi
+
+# Modify history sizes before changing location to avoid unintentionally
+# truncating the history file early.
+
+# "Numeric values less than zero result in every command being saved on the history list (there is no limit)"
+HISTSIZE=-1 2> /dev/null || true
+
+# "Non-numeric values and numeric values less than zero inhibit truncation"
+HISTFILESIZE='unlimited' 2> /dev/null || true
+
+# Use a custom history file location so history is not truncated
+# if the environment ever loses this "eternal" configuration.
+HISTDIR="${XDG_STATE_HOME:-${HOME?}/.local/state}/bash"
+[[ -d ${HISTDIR?} ]] || mkdir -p "${HISTDIR?}"
+HISTFILE="${HISTDIR?}/history" 2> /dev/null || true
+
 
 # @function     hr
 # @description  outputs the history of commands used sorted by usage and count
